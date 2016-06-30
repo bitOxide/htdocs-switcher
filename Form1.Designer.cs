225735351ccae709e1c999a1d45c72257c016be7
Christian Wischenbart <c.wischenbart@bitoxide.com>
2016-06-30 16:02:47 +0200

    foo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,133 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(90, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "htdocs Switcher";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 71);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(91, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(272, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Wähle ein Verzeichnis aus der Liste aus.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(94, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(313, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 99);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(380, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(40, 17);
+            this.toolStripStatusLabel1.Text = "Bereit.";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(380, 121);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "htdoc Switcher";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,188 @@ namespace htdocsSwitcher
 {
     public partial class Form1 : Form
     {
+        private readonly string configFile = "config.txt";
+        private string xamppLocation;
+        private IDictionary<string, string> projects;
+
         public Form1()
         {
             InitializeComponent();
+            RefreshAll();
+        }
+
+        private void Log(string txt)
+        {
+            Debug.WriteLine(txt);
+            toolStripStatusLabel1.Text = txt;
+            statusTimerReset();
+        }
+
+        private void statusTimerReset()
+        {
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private void Switch()
+        {
+            if (comboBox1.SelectedItem == null) return;
+            if (xamppLocation == null) return;
+
+            var n = comboBox1.SelectedItem.ToString();
+            if (!projects.ContainsKey(n))
+            {
+                Log("ProjektID konnte nicht gefunden werden.");
+                return;
+            }
+            var loc = projects[n];
+
+            var htdocsLoc = Path.Combine(xamppLocation, "htdocs");
+            var isJunction = JunctionPoint.Exists(htdocsLoc);
+            var isDirOrFile = Directory.Exists(htdocsLoc) || File.Exists(htdocsLoc);
+
+            if (!isJunction && isDirOrFile)
+            {
+                Log("htdocs existiert bereits.");
+                return;
+            }
+
+            JunctionPoint.Create(htdocsLoc, loc, true);
+            Log($"{n} erfolgreich verknüpft.");
+        }
+
+        private void RefreshAll()
+        {
+            RefreshEntries();
+            UpdateDropDown();
+        }
+
+        private void ClearEntries()
+        {
+            xamppLocation = null;
+            projects = null;
+        }
+
+        private void UpdateDropDown()
+        {
+            comboBox1.BeginUpdate();
+            try
+            {
+                comboBox1.Items.Clear();
+
+                if (xamppLocation == null) return;
+                if (projects == null) return;
+
+                foreach (var p in projects)
+                {
+                    comboBox1.Items.Add(p.Key);
+                }
+            }
+            finally
+            {
+                comboBox1.EndUpdate();
+            }
+        }
+
+        private void RefreshEntries()
+        {
+            string[] fileLines = null;
+
+            if (!File.Exists(configFile))
+            {
+                Log("config.txt nicht gefunden.");
+                ClearEntries();
+                return;
+            }
+
+            try { fileLines = File.ReadAllLines(configFile); }
+            catch (Exception ex)
+            {
+                Log("Fehler config.txt: " + ex.Message);
+                ClearEntries();
+                return;
+            }
+
+            //fileLines = new string[]
+            //{
+            //    @"D:\Xampp",
+            //    @"D:\Code\bitOxide\SimplePoke\src|SimplePoke",
+            //    @"D:\Xampp\htdocs.default",
+            //};
+
+            if (fileLines.Length < 2)
+            {
+                Log("Keine XAMPP Path bzw. Projekt gefunden.");
+                ClearEntries();
+                return;
+            }
+
+            xamppLocation = fileLines[0].Trim();
+            if (!Directory.Exists(xamppLocation))
+            {
+                Log("XAMPP Path ungültig.");
+                ClearEntries();
+                return;
+            }
+
+            var dict = new Dictionary<string, string>();
+
+            foreach (var l in fileLines.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(l))
+                {
+                    Debug.WriteLine("Leere Zeile übersprungen");
+                    continue;
+                }
+
+                var parts = l.Split('|');
+                if (parts.Length > 2)
+                {
+                    Debug.WriteLine("Ungültiges Format (nur eine |-Trennlinie erlaubt).");
+                    continue;
+                }
+
+                string location = null;
+                string name = null;
+
+                if (parts.Length == 1)
+                {
+                    location = parts[0];
+                    name = Path.GetFileName(location);
+                }
+                else
+                {
+                    location = parts[0];
+                    name = parts[1];
+                }
+
+                if (!Directory.Exists(location))
+                {
+                    Debug.WriteLine("Projektpfad nicht gefunden.");
+                    continue;
+                }
+
+                dict.Add(name, location);
+                Debug.WriteLine($"Projekt \"{name}\" hinzugefügt.");
+            }
+
+            projects = dict;
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            MessageBox.Show(Properties.Resources.helpText, "Aufbau \"config.txt\"", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Switch();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Bereit.";
         }
     }
 }
